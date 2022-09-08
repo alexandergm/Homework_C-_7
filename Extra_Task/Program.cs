84d6679b9a6579f8cf140ddc64a44fb11a7fa01f
Alexander_Vasilyev <sasha_261@outlook.com>
2022-09-08 23:46:08 +0300

    Initial commit

---
+++ Extra_Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Extra_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
