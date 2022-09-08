4df61d4d95375af7080f997841b71de01cb8e976
Alexander_Vasilyev <sasha_261@outlook.com>
2022-09-09 01:23:37 +0300

    Complete Extra_Task

--- Extra_Task/Program.cs
+++ Extra_Task/Program.cs
@@ -6,7 +6,61 @@ namespace Extra_Task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите количество строк массива: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов массива: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] array = new int[m,n];
+            PrintArray(FillArray(array));
+            SortArray();
+            Console.WriteLine("Отсортированный массив: ");
+            PrintArray(array);
+
+            int[,] FillArray(int[,] arr)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        arr[i,j] = new Random().Next(1,100);
+                    }
+                }
+                return arr;
+            }
+            void PrintArray(int[,] arr)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        Console.Write(arr[i,j] + "\t" );
+                    }
+                    Console.WriteLine();
+                }
+            }
+            void SortArray()
+            {
+                int arrayMaxLength;
+                if(m > n) arrayMaxLength = m;
+                else arrayMaxLength = n;                
+                int[] temp = new int[arrayMaxLength];
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                { 
+                    for (int j = 0; j < array.GetLength(1); j++)  temp[j] = array[i, j];                        
+                    Array.Sort(temp);
+
+                    for (int k = 0; k < array.GetLength(1); k++) array[i, k] = temp[k];                                     
+                }    
+                
+                for (int i = 0; i < array.GetLength(1); i++)
+                {
+                    for (int j = 0; j < array.GetLength(0); j++)  temp[j] = array[j, i];                        
+                    Array.Sort(temp);
+
+                    for (int k = 0; k < array.GetLength(0); k++) array[k, i] = temp[k];                        
+                }
+            }
         }
     }
 }
