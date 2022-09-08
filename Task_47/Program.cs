851ce560afddb64d39f928fb192e8e0a3a9beb0d
Alexander_Vasilyev <sasha_261@outlook.com>
2022-09-09 00:00:47 +0300

    Complete Task_47

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -6,7 +6,35 @@ namespace Task_47
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите количество строк массива: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов массива: ");
+            int n = int.Parse(Console.ReadLine());
+            double[,] array = new double[m,n];
+            PrintArray(FillArray(array));
+
+            double[,] FillArray(double[,] arr)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        arr[i,j] = new Random().Next(1,100) + Math.Round(new Random().NextDouble() , 2);
+                    }
+                }
+                return arr;
+            }
+            void PrintArray(double[,] arr)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        Console.Write(arr[i,j] + "\t" );
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
