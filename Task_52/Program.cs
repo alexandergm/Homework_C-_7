6e075a50b20c0f4090559c9e492bfbdbef0a4c93
Alexander_Vasilyev <sasha_261@outlook.com>
2022-09-09 00:38:52 +0300

    Complete Task_52

--- Task_52/Program.cs
+++ Task_52/Program.cs
@@ -6,7 +6,49 @@ namespace Task_52
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите количество строк массива: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов массива: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] array = new int[m,n];
+            PrintArray(FillArray(array));
+            ArifmeticAverage();
+
+            int[,] FillArray(int[,] arr)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        arr[i,j] = new Random().Next(1,100);
+                    }
+                }
+                return arr;
+            }
+            void PrintArray(int[,] arr)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        Console.Write(arr[i,j] + "\t" );
+                    }
+                    Console.WriteLine();
+                }
+            }
+            void ArifmeticAverage()
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    double average = 0;
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        average += array[j, i];
+                    }
+                    average = average / array.GetLength(1);
+                    Console.WriteLine($"Arifmetic average in {i} column: {average}");
+                }
+            }
         }
     }
 }
